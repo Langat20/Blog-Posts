dfccfd00b73e06c82f04d5a23c82a1688bf05539
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-15 16:58:43 +0300

    defined the login method

--- app/auth/views.py
+++ app/auth/views.py
@@ -26,6 +26,22 @@ def register():
 
 
 
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.homepage'))
+
+        flash('Invalid username or Password')
+
+    
+    return render_template('auth/login.html',login_form = login_form)
+
+
+
 
 
     
