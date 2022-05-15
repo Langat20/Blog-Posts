2af1408e6522db7abc0cef2114af33ce7dbadc20
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-15 17:10:08 +0300

    add users table

---
+++ app/models.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+from . import db,login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    blogs= db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship("Comments", backref="user", lazy="dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_user(cls, id):
+        pitches= User.query.filter_by(id=id).all()
+        return pitches
+
+
+    def __repr__(self):
+        return f'User{self.username}'
+
+
+
+
+
