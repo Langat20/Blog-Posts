fd9cb07eb485ed4f3c9b4f7169d0212e29c7ff00
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-16 18:40:14 +0300

    deployment

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+from distutils.debug import DEBUG
 import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI ='postgres://pnrobfjltapagu:e8922689e3080afad5c58191c70dae74e4003a7911375b5e0baba6a71c3f0cd9@ec2-54-86-224-85.compute-1.amazonaws.com:5432/db4auqgv9jj3an'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:langat20@localhost/blogs'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:langat20@localhost/blogs'
     SECRET_KEY='qwertyqwerty'
 
     # email configurations
@@ -16,26 +17,26 @@ class Config:
     # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
-    class ProdConfig(Config):
         '''
     Production  configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
     '''
 
-    uri = os.getenv("DATABASE_URL")  # or other relevant config var
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
+        uri = os.getenv("DATABASE_URL")  # or other relevant config var
+        if uri.startswith("postgres://"):
+            uri = uri.replace("postgres://", "postgresql://", 1)
 
-    SQLALCHEMY_DATABASE_URI=uri
+        SQLALCHEMY_DATABASE_URI=uri
 
 DEBUG = True
-
-
+   
 
 class DevConfig(Config):
-    DEBUG = True
+    
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:langat20@localhost/blogs'
 
+    DEBUG=True
 
 config_options = {
 'development':DevConfig,
